52b1dec9159f0cca8caf8bc4113a9bef22787614
japguele <japguele@student.avans.nl>
2015-04-01 18:17:50 +0200

    test if heroku is fucking up

--- app.js
+++ app.js
@@ -51,11 +51,6 @@ var isAuthenticated = function (req, res, next) {
     res.redirect('/login');
 };
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 var loginRoutes = require('./routes/login.js')(router, passport);
 
