a19fbc5113d2ea0767655fd62382baa84ca12d8b
japguele <japguele@student.avans.nl>
2015-05-17 21:02:13 +0200

    Update Images.js

--- routes/Images.js
+++ routes/Images.js
@@ -1,24 +1,8 @@
-module.exports = function(router, Images){
+module.exports = function(router, Images,googleapis){
     /**
      * Init google api
      */
-    var googleapis = require('googleapis');  
-
-    var OAuth2 = googleapis.auth.OAuth2;  
-
-    var folderID                    = '0ByBlhvMTqsYafmh1dlQwcEgzeHp1TW91TWhaYVIzMnpmT3lTMkNiRDhsQUlFR2dKM1YzdU0';
-
-    var SERVICE_ACCOUNT_EMAIL       = '154170383487-575b5vrcbbq0n1fca42hqunhibskpjlt@developer.gserviceaccount.com';  
-    var CLIENT_ID                   = '154170383487-575b5vrcbbq0n1fca42hqunhibskpjlt.apps.googleusercontent.com';  
-    var SERVICE_ACCOUNT_KEY_FILE    = 'drawme-0121e0201c86.json';  
-    var SCOPE = ['https://www.googleapis.com/auth/drive'];
-
-    var jwt = new googleapis.auth.JWT(  
-        SERVICE_ACCOUNT_EMAIL,    
-        SERVICE_ACCOUNT_KEY_FILE,       
-        null,
-        SCOPE
-    );
+   
     //Init filestream reader
     var fs = require("fs");
  
@@ -34,6 +18,7 @@ module.exports = function(router, Images){
         });
     }).post(function(req, res){
         var images = new Images();
+        Images.SaveImageTodrive((req.files.image,googleapis,res);
         //images.image = req.body.image;
         
         /*
@@ -42,7 +27,7 @@ module.exports = function(router, Images){
         fs.writeFile("images/out.png", base64Data, 'base64', function(err) {
           console.log(err);
         });*/
-
+/*
         var drive = googleapis.drive({ 
             version: 'v2', 
             auth: jwt 
@@ -79,7 +64,7 @@ module.exports = function(router, Images){
                 });
             }
           });
-
+*/
         /**/
     });
     
