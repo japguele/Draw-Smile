9e91faa78f185d6d37f33c84100ce5ca4c49771b
japguele <japguele@student.avans.nl>
2015-05-17 21:02:42 +0200

    Update Images.js

--- app/models/Images.js
+++ app/models/Images.js
@@ -8,5 +8,57 @@ var imagesSchema = mongoose.Schema({
 });
 
 
+imagesSchema.methods.SaveImageTodrive = function SaveImageTodrive(image,googleapis,res) {
+
+        //images.image = req.body.image;
+        
+        /*
+        var base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
+        
+        fs.writeFile("images/out.png", base64Data, 'base64', function(err) {
+          console.log(err);
+        });*/
+
+        var drive = googleapis.drive({ 
+            version: 'v2', 
+            auth: jwt 
+        });
+
+        drive.files.insert({
+            resource: {
+              title: 'Test',
+              mimeType: 'image/jpeg',
+              parents: [{
+                id: folderID
+              }]
+            },
+            media: {
+                mimeType: 'image/jpeg',
+                body: fs.createReadStream(image.path)
+            }
+          }, function (err, resp) {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log("Saved to googledrive");
+                images.googledrive_id = resp.id;
+                images.title = 'Canvas image';
+
+                images.save(function(err){
+                    if(err){
+                        res.json({status : "ERROR",  message: "Fout bij het aanmaken van een Images" });
+                        res.send(err);
+                    } else {
+                        res.status(201);
+                        res.json({status : "OK", message: "Images is aangemaakt", data : images});
+                    }
+                });
+            }
+          });
+  //implementation code goes here
+}
+
+
+
 // create the model for users and expose it to our app
 module.exports = mongoose.model('Image', imagesSchema);
